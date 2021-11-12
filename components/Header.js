760a554d19b4fb505a624ce6a456ad42caa25bf4
Alexander Horn <alexhorn@users.noreply.github.com>
2021-11-12 21:32:59 +0100

    Change title to include entire council

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,12 @@ export default function Header () {
   return (
     <>
       <Head>
-        <title>Meckerkasten der Fachschaft Informatik</title>
+        <title>Meckerkasten der Studierendenvertretung</title>
         <meta name="description" content="Meckerkasten von der Fachschaft Informatik für die Studierenden der Fakultät Informatik" />
         <link rel="icon" type="image/svg+xml" href="https://assets.neuland.app/StudVer_Logo_ohne%20Schrift.svg" />
       </Head>
       <img src="https://assets.neuland.app/StudVer_Logo_2020_CMYK.svg" width="300" className="mb-3" />
-      <h1>Meckerkasten der Fachschaft Informatik</h1>
+      <h1>Meckerkasten der Studierendenvertretung</h1>
     </>
   )
 }
